0b49d0d2d54edd92da216f76dbb29663f0fad98f
Michael Taye <michaeltaye200@gmail.com>
2026-05-27 11:55:54 +0300

    Update ENV

--- src/config/env.js
+++ src/config/env.js
@@ -1,25 +1,46 @@
 import Constants from 'expo-constants';
 
-export const env = {
-  apiUrl:
-    Constants.expoConfig?.extra?.apiUrl ||
-    process.env.EXPO_PUBLIC_API_URL || '',
+const LIVE_API_URL = 'https://taxiapi.zmichael.click/api/v1';
+const LIVE_SOCKET_URL = 'https://taxiapi.zmichael.click';
+const LIVE_OSRM_ADDIS_URL = 'https://taxiapi.zmichael.click/osrm/addis';
+const LIVE_OSRM_BAHIRDAR_URL = 'https://taxiapi.zmichael.click/osrm/bahirdar';
 
-  socketUrl:
-    Constants.expoConfig?.extra?.socketUrl ||
-    process.env.EXPO_PUBLIC_SOCKET_URL || '',
+const isLocalUrl = (value = '') =>
+  /^(https?:\/\/)?(localhost|127\.0\.0\.1|10\.0\.2\.2|192\.168\.|10\.|172\.(1[6-9]|2\d|3[0-1])\.)/i.test(value);
+
+const productionUrl = (value, fallback) => {
+  if (__DEV__) return value || fallback;
+  return value && !isLocalUrl(value) ? value : fallback;
+};
+
+const rawApiUrl =
+  Constants.expoConfig?.extra?.apiUrl ||
+  process.env.EXPO_PUBLIC_API_URL || '';
+
+const rawSocketUrl =
+  Constants.expoConfig?.extra?.socketUrl ||
+  process.env.EXPO_PUBLIC_SOCKET_URL || '';
+
+const rawOsrmAddisUrl =
+  Constants.expoConfig?.extra?.osrmAddisUrl ||
+  process.env.EXPO_PUBLIC_OSRM_ADDIS_URL || '';
+
+const rawOsrmBahirdarUrl =
+  Constants.expoConfig?.extra?.osrmBahirdarUrl ||
+  process.env.EXPO_PUBLIC_OSRM_BAHIRDAR_URL || '';
+
+export const env = {
+  apiUrl: productionUrl(rawApiUrl, LIVE_API_URL),
+
+  socketUrl: productionUrl(rawSocketUrl, LIVE_SOCKET_URL),
 
   googleMapsKey:
     Constants.expoConfig?.extra?.googleMapsKey ||
     process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY || '',
 
-  osrmAddisUrl:
-    Constants.expoConfig?.extra?.osrmAddisUrl ||
-    process.env.EXPO_PUBLIC_OSRM_ADDIS_URL || '',
+  osrmAddisUrl: productionUrl(rawOsrmAddisUrl, LIVE_OSRM_ADDIS_URL),
 
-  osrmBahirdarUrl:
-    Constants.expoConfig?.extra?.osrmBahirdarUrl ||
-    process.env.EXPO_PUBLIC_OSRM_BAHIRDAR_URL || '',
+  osrmBahirdarUrl: productionUrl(rawOsrmBahirdarUrl, LIVE_OSRM_BAHIRDAR_URL),
 };
 
 if (__DEV__) {
